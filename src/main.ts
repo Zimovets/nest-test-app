97577d600bd582d1371f654bf627300ed3aa4333
Zimovets Serhii <zimovets.s.a@gmail.com>
2023-03-02 22:53:02 +0200

    error message enable

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './core/exceptions/allExceptionsFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // global prefix
   app.setGlobalPrefix('api/v1');
+
+  const adapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(adapterHost));
+
   await app.listen(3000);
 }
 bootstrap();
